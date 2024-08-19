cf1792870c96996f28c863d24c24dbed333e7a95
Alicja Stefanska <alicja.stefanska44@gmail.com>
2024-08-19 17:35:39 +0200

    finish version

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -12,8 +12,7 @@
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
-# define MAX_INT INT_MAX
-# define MIN_INT INT_MIN
+
 # include <unistd.h>
 # include <string.h>
 # include <stdio.h>
@@ -22,41 +21,48 @@
 # include <limits.h>
 
 //OPERATIONS
-void	push_stack_a(long *stack_a, long *stack_b,
-			int *num_in_a, int *num_in_b);
-void	push_stack_b(long *stack_a, long *stack_b,
-			int *num_in_a, int *num_in_b);
-void	rev_rot_stack_a(long *stack_a, int *num_in_a);
-void	rev_rot_stack_b(long *stack_b, int *num_in_b);
-void	rev_a_and_b(long *stack_a, long *stack_b, int *num_in_a, int *num_in_b);
-void	rotate_stack_a(long *stack_a, int *num_in_a);
-void	rotate_stack_b(long *stack_b, int *num_in_b);
-void	rot_a_and_b(long *stack_a, long *stack_b, int *num_in_a, int *num_in_b);
-void	swap_stack_a(long *stack_a, int *num_in_a);
-void	swap_stack_b(long *stack_b, int *num_in_b);
-void	swap_stack_a_and_b(long *stack_a, long *stack_b,
-			int *num_in_a, int *num_in_b);
+void			push_stack_a(long *stack_a, long *stack_b,
+					int *num_in_a, int *num_in_b);
+void			push_stack_b(long *stack_a, long *stack_b,
+					int *num_in_a, int *num_in_b);
+void			rev_rot_stack_a(long *stack_a, int *num_in_a);
+void			rev_rot_stack_b(long *stack_b, int *num_in_b);
+void			rev_a_and_b(long *stack_a, long *stack_b,
+					int *num_in_a, int *num_in_b);
+void			rotate_stack_a(long *stack_a, int *num_in_a);
+void			rotate_stack_b(long *stack_b, int *num_in_b);
+void			rot_a_and_b(long *stack_a, long *stack_b,
+					int *num_in_a, int *num_in_b);
+void			swap_stack_a(long *stack_a, int *num_in_a);
+void			swap_stack_b(long *stack_b, int *num_in_b);
+void			swap_stack_a_and_b(long *stack_a, long *stack_b,
+					int *num_in_a, int *num_in_b);
 //SORTING
-void	sort_3_num(long *stack_a, int *num_in_a);
-void	sort_5_num(long *stack_a, long *stack_b, int *num_in_a, int *num_in_b);
-void	sort_other(long *stack_a, long *stack_b, int *num_in_a, int *num_in_b);
+void			sort_3_num(long *stack_a, int *num_in_a);
+void			sort_5_num(long *stack_a, long *stack_b,
+					int *num_in_a, int *num_in_b);
+void			sort_other(long *stack_a, long *stack_b,
+					int *num_in_a, int *num_in_b);
 //UTILS
-void	free_stacks(long *stack_a, long *stack_b, int *num_in_a, int *num_in_b);
-void	ft_new_allocation(long **tab, int startsize, int newsize);
-void	free_str(char **str);
-void	operations(long *stack_a, long *stack_b, int *num_in_a, int *num_in_b);
-int		stack_size(int argc, char **argv);
-int		duplicates(long *stack_a, int num_in_a);
-int		ft_strlen(const char *str);
-int		is_digit(int i);
-int		only_numbers(char *num);
-int		number_limit(long *stack_a, int num_in_a);
-int		is_stack_is_sorted(long *stack_a, int *num_in_a);
+void			free_stacks(long *stack_a, long *stack_b,
+					int *num_in_a, int *num_in_b);
+void			ft_new_allocation(long **tab, int startsize, int newsize);
+void			free_str(char **str);
+void			operations(long *stack_a, long *stack_b,
+					int *num_in_a, int *num_in_b);
+int				stack_size(int argc, char **argv);
+int				duplicates(long *stack_a, int num_in_a);
+int				ft_strlen(const char *str);
+int				is_digit(int i);
+int				only_numbers(char *num);
+int				number_limit(long *stack_a, int num_in_a);
+int				is_stack_is_sorted(long *stack_a, int *num_in_a);
 long long int	ft_atoi_long(const char *str);
-int		numbers_input(long *stack_a, int *num_in_a,
-			char **argv, char **numbers_list);
-int		check_separated_input(long *stack_a, int *num_in_a, char **argv);
-int		separating_input(long *stack_a, int *num_in_a, char **argv);
-char	**ft_split(const char *str, char c);
+int				numbers_input(long *stack_a, int *num_in_a,
+					char **argv, char **numbers_list);
+int				check_separated_input(long *stack_a,
+					int *num_in_a, char **argv);
+int				separating_input(long *stack_a, int *num_in_a, char **argv);
+char			**ft_split(const char *str, char c);
 
 #endif
